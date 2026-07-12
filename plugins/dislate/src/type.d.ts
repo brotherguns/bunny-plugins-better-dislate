d88ef4ff71d1922110b9a2538416efa8b8bdf6b6
brotherguns <187798204+brotherguns@users.noreply.github.com>
2026-07-12 08:08:03 -0500

    Add files via upload

--- plugins/dislate/src/type.d.ts
+++ plugins/dislate/src/type.d.ts
@@ -9,6 +9,10 @@ export interface GTranslateResponse {
     src?: string;
     sentences?: { trans?: string }[];
 }
+export interface GeminiResponse {
+    candidates?: { content?: { parts?: { text?: string }[] } }[]
+    error?: { message?: string }
+}
 export interface TranslateResult {
     source_lang: string
     text: string
